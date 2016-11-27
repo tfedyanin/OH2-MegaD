15f9cb2990babc557684d59f65175e7fb4facc91
tim06ka <fedyanint@gmail.com>
2016-11-27 21:35:35 +0300

    Мелкие правки

--- org.openhab.binding.megad/src/main/java/org/openhab/binding/megad/MegaDBindingConstants.java
+++ org.openhab.binding.megad/src/main/java/org/openhab/binding/megad/MegaDBindingConstants.java
@@ -7,22 +7,60 @@
  */
 package org.openhab.binding.megad;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 
 /**
- * The {@link MegaDBinding} class defines common constants, which are 
+ * The {@link MegaDBinding} class defines common constants, which are
  * used across the whole binding.
- * 
+ *
  * @author Timofey Fedyanin - Initial contribution
  */
 public class MegaDBindingConstants {
 
     public static final String BINDING_ID = "megad";
-    
+
     // List of all Thing Type UIDs
-    public final static ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+    public final static ThingTypeUID THING_TYPE_MEGAD = new ThingTypeUID(BINDING_ID, "megad-thing");
 
     // List of all Channel ids
-    public final static String CHANNEL_1 = "channel1";
 
+    public final static String P01_IN = "P01-IN";
+    public final static String P02_IN = "P02-IN";
+    public final static String P03_IN = "P03-IN";
+    public final static String P04_IN = "P04-IN";
+    public final static String P05_IN = "P05-IN";
+    public final static String P06_IN = "P06-IN";
+    public final static String P07_OUT = "P07-OUT";
+    public final static String P08_OUT = "P08-OUT";
+    public final static String P09_OUT = "P09-OUT";
+    public final static String P10_OUT = "P10-OUT";
+    public final static String P11_OUT = "P11-OUT";
+    public final static String P12_OUT = "P12-OUT";
+    public final static String P13_OUT = "P13-OUT";
+
+    public final static String P_PORT = "port";
+    public final static String P_ADDRESS = "address";
+    public final static String P_PASSWORD = "password";
+    public final static String P_TIMEOUT = "timeout";
+
+    public final static Map<String, Integer> CHANNEL = new HashMap<String, Integer>() {
+        {
+            put(P01_IN, 1);
+            put(P02_IN, 2);
+            put(P03_IN, 3);
+            put(P04_IN, 4);
+            put(P05_IN, 5);
+            put(P06_IN, 6);
+            put(P07_OUT, 7);
+            put(P08_OUT, 8);
+            put(P09_OUT, 9);
+            put(P10_OUT, 10);
+            put(P11_OUT, 11);
+            put(P12_OUT, 12);
+            put(P13_OUT, 13);
+        }
+    };
 }
--- org.openhab.binding.megad/src/main/java/org/openhab/binding/megad/handler/MegaDHandler.java
+++ org.openhab.binding.megad/src/main/java/org/openhab/binding/megad/handler/MegaDHandler.java
@@ -9,44 +9,59 @@ package org.openhab.binding.megad.handler;
 
 import static org.openhab.binding.megad.MegaDBindingConstants.*;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.State;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * The {@link MegaDHandler} is responsible for handling commands, which are
  * sent to one of the channels.
- * 
+ *
  * @author Timofey Fedyanin - Initial contribution
  */
 public class MegaDHandler extends BaseThingHandler {
-
     private Logger logger = LoggerFactory.getLogger(MegaDHandler.class);
+    // private MegaD megad;
 
-	public MegaDHandler(Thing thing) {
-		super(thing);
-	}
+    public MegaDHandler(Thing thing) {
+        super(thing);
+    }
 
-	@Override
-	public void handleCommand(ChannelUID channelUID, Command command) {
-        if(channelUID.getId().equals(CHANNEL_1)) {
-            // TODO: handle command
-
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        try {
+            Integer channel = CHANNEL.get(channelUID.getId());
+            if (channel != null) {
+                if (command.equals(OnOffType.ON)) {
+                    // megad.turnOn(channel);
+                }
+                if (command.equals(OnOffType.OFF)) {
+                    // megad.turnOff(channel);
+                }
+            }
+        } catch (Throwable ignored) {
         }
-	}
+    }
 
     @Override
     public void initialize() {
         // TODO: Initialize the thing. If done set status to ONLINE to indicate proper working.
         // Long running initialization should be done asynchronously in background.
+        int port = ((BigDecimal) getThing().getConfiguration().get(P_PORT)).intValue();
+        String host = getThing().getConfiguration().get(P_ADDRESS).toString();
+        String pass = getThing().getConfiguration().get(P_PASSWORD).toString();
+        int timeout = ((BigDecimal) getThing().getConfiguration().get(P_TIMEOUT)).intValue();
+
+        // megad = new MegaD(host, pass, port, timeout);
         updateStatus(ThingStatus.ONLINE);
 
         // Note: When initialization can NOT be done set the status with more details for further
@@ -56,4 +71,41 @@ public class MegaDHandler extends BaseThingHandler {
         // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
         // "Can not access device as username and/or password are invalid");
     }
+
+    @Override
+    public void handleConfigurationUpdate(Map<String, Object> configurationParameters) {
+        // TODO Auto-generated method stub
+        super.handleConfigurationUpdate(configurationParameters);
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        super.dispose();
+    }
+
+    @Override
+    public void handleUpdate(ChannelUID channelUID, State newState) {
+        // TODO Auto-generated method stub
+        super.handleUpdate(channelUID, newState);
+    }
+
+    @Override
+    public void thingUpdated(Thing thing) {
+        // TODO Auto-generated method stub
+        super.thingUpdated(thing);
+    }
+
+    @Override
+    public void channelLinked(ChannelUID channelUID) {
+        // TODO Auto-generated method stub
+        super.channelLinked(channelUID);
+    }
+
+    @Override
+    public void channelUnlinked(ChannelUID channelUID) {
+        // TODO Auto-generated method stub
+        super.channelUnlinked(channelUID);
+    }
+
 }
